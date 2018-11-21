56f5695845710e108bec2e22ed32469181c582eb
Jaeyoung <whwodud9@gmail.com>
2018-11-21 20:58:40 +0900

    Update counting mol function. Add comment.

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,7 +1,9 @@
 import os
 from os.path import isfile, join
+import urllib.request
 import multiprocessing as mp
 import time
+from tqdm import tqdm
 
 # ==== Global Variables ==== #
 mypath = './dataset/zinc_smiles'
@@ -14,10 +16,11 @@ total_file = 0
 ts = time.time()
 
 # ==== Downloading Function ==== #
-def download(url):
+def download(url, onlyfiles):
     name = url.split('/')[-1].strip() # filename would be AAAC.smi, AEBC.smi ...
     if name not in onlyfiles: # download file only when the file does not exist.
-        os.system('wget -q -P {} {}'.format('./dataset/zinc_smiles', url))
+#         os.system('wget -q -P {} {}'.format('./dataset/zinc_smiles', url))
+        urllib.request.urlretrieve(url, './dataset/zinc_smiles/{}'.format(name))
         return name, True
     return name, False
 
@@ -49,29 +52,49 @@ def log_result(output):
         print("Failed     {:8}. included {:9} mol. {:4}/{:4}. elapsed {:5.1f} sec".format(name, cnt, cnt_file, total_file, took))
 
 
-def save_smi(filename='ZINC-downloader-2D-smi.uri'):
+def save_smi(start_offset=0, filename='ZINC-downloader-2D-smi.uri'):
     ts = time.time()
+    global cnt_file, total_file
+    cnt_file += start_offset
 
     with open(filename) as file:
         list_url = file.readlines()
+        total_file = len(list_url)
 
-    global total_file
-    total_file = len(list_url)
-
-    pool = mp.Pool(processes=40)
+    # Reset dataset folder
     # os.system('rm -rf ./dataset/zinc_smiles')
     # os.system('mkdir ./dataset/zinc_smiles')
-    for url in list_url:
-        pool.apply_async(download, args = (url,), callback=log_result)
+
+    # Start Downloading Process
+    pool = mp.Pool(processes=10)
+    for url in list_url[start_offset:]:
+        time.sleep(0.05)
+        pool.apply_async(download, args = (url, onlyfiles), callback=log_result)
     pool.close()
     pool.join()
     te = time.time()
 
+    # Report Work
     print("=================================================================================")
-    print("Download Completed!   {} Molecular retrieved from   {}/{} files. Took {:5.1f} sec".format(cnt_mol, total_file-cnt_fail, total_file, te-ts ))
+    print("Downloading Completed! {} Molecular retrieved from   {}/{} files. Took {:5.1f} sec".format(cnt_mol, total_file-cnt_fail, total_file, te-ts ))
+    print("=================================================================================")
+
+def count_mol():
+    mypath = './dataset/zinc_smiles'
+    onlyfiles = [f for f in os.listdir(mypath) if isfile(join(mypath, f))]
+    print("=================================================================================")
+    print("Counting Started!")
+    print("=================================================================================")
+
+    cnt_mol = 0
+    for filename in tqdm(onlyfiles):
+        cnt_mol += countline(join(mypath, filename))
+
+    print("=================================================================================")
+    print("Counting Completed!   {} Molecular retrieved from {} files.".format(cnt_mol, len(onlyfiles)))
     print("=================================================================================")
 
 
 if __name__ == '__main__':
-
-    save_smi()
+    save_smi(start_offset=0)
+    count_mol()
