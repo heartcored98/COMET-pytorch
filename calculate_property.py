5bb257f3e2f8c9683f70ae48a1dcf70ba72a2d9f
Jaeyoung <whwodud9@gmail.com>
2018-11-25 02:36:34 +0900

    Smile parsing error handling added. if error occured then None return Pandas.dropna would delete the row including nan

--- calculate_property.py
+++ calculate_property.py
@@ -13,9 +13,12 @@ import pandas as df
 
 
 def process_smile(row):
-    smi = row.split(' ')[0].strip()
-    m = Chem.MolFromSmiles(smi)
-    return (smi, MolLogP(m), MolMR(m), CalcTPSA(m), m.GetNumAtoms())
+    try:
+        smi = row.split(' ')[0].strip()
+        m = Chem.MolFromSmiles(smi)
+        return (smi, MolLogP(m), MolMR(m), CalcTPSA(m), m.GetNumAtoms())
+    except:
+        return (None, None, None, None, None)
 
 def process_dataset(chunk_size=10000, #500000,
                     num_worker=15,
@@ -36,8 +39,10 @@ def process_dataset(chunk_size=10000, #500000,
     val_row_buffer = list()
     label_columns = ('smile', 'logP', 'mr', 'tpsa', 'length')
 
+    target_list_file = list_file[start_offset:end_offset]
 
-    for idx, filename in enumerate(list_file[start_offset:end_offset][:10]):
+
+    for idx, filename in enumerate(target_list_file):
         m_ts = time.time()
         with open(join(raw_dir_path, filename)) as file:
             list_row = file.readlines()[1:]
@@ -50,12 +55,13 @@ def process_dataset(chunk_size=10000, #500000,
         cnt_val_mol += len(val_data)
         m_te = time.time()
         print("Processed {:4}/{:4} files of {:8} mols took {:5.1f} sec. {:7.1f} mol/sec. Train: {:4} Val: {:4} Elap: {:6.1f} sec".format(
-            idx, len(list_file), len(data), m_te-m_ts, len(data)/(m_te-m_ts), cnt_train_chunk, cnt_val_chunk, time.time()-ts))
+            idx+1, len(target_list_file), len(data), m_te-m_ts, len(data)/(m_te-m_ts), cnt_train_chunk, cnt_val_chunk, time.time()-ts))
 
         while len(train_row_buffer) > chunk_size:
             if len(train_row_buffer) >= chunk_size:
                 output_train = train_row_buffer[:chunk_size]
                 df_train = df.DataFrame.from_records(output_train, columns=label_columns)
+                df_train = df_train.dropna()
                 df_train.to_csv(path_or_buf=join(output_dir_path, 'train/train{:06d}.csv'.format(cnt_train_chunk)), float_format='%g', index=False)
                 train_row_buffer = train_row_buffer[chunk_size:]
                 cnt_train_chunk += 1
@@ -63,6 +69,7 @@ def process_dataset(chunk_size=10000, #500000,
             if len(val_row_buffer) >= chunk_size:
                 output_val = val_row_buffer[:chunk_size]
                 df_val = df.DataFrame.from_records(output_val, columns=label_columns)
+                df_val = df_val.dropna()
                 df_val.to_csv(path_or_buf=join(output_dir_path, 'val/val{:06d}.csv'.format(cnt_val_chunk)), float_format='%g', index=False)
                 val_row_buffer = val_row_buffer[chunk_size:]
                 cnt_val_chunk += 1
@@ -70,9 +77,11 @@ def process_dataset(chunk_size=10000, #500000,
 
 
     df_train = df.DataFrame.from_records(train_row_buffer, columns=label_columns)
+    df_train = df_train.dropna()
     df_train.to_csv(path_or_buf=join(output_dir_path, 'train/train{:06d}.csv'.format(cnt_train_chunk)),
                         float_format='%g', index=False)
     df_val = df.DataFrame.from_records(val_row_buffer, columns=label_columns)
+    df_val = df_val.dropna()
     df_val.to_csv(path_or_buf=join(output_dir_path, 'val/val{:06d}.csv'.format(cnt_val_chunk)), float_format='%g',
                   index=False)
 
@@ -80,17 +89,17 @@ def process_dataset(chunk_size=10000, #500000,
 
     # Report Work
     print("======================================================================================")
-    print("Processing Completed! Train : {} mols. Val : {} mols.  Took {:5.1f}. {} mol/sec".format(cnt_train_mol, cnt_val_mol, te-ts, (cnt_train_mol+cnt_val_mol)/(te-ts)))
+    print("Processing Completed! Train : {} mols. Val : {} mols.  Took {:5.1f} sec. {:5.1f} mol/sec".format(cnt_train_mol, cnt_val_mol, te-ts, (cnt_train_mol+cnt_val_mol)/(te-ts)))
     print("======================================================================================")
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Add logP, TPSA, MR, PBF value on .smi files')
-    parser.add_argument("-c", "--chunk_size", help="number of rows in one chunk ", type=int, default=100000)
+    parser.add_argument("-c", "--chunk_size", help="number of rows in one chunk ", type=int, default=25000000)
     parser.add_argument("-n", "--num_worker", help="number of co-working process", type=int, default=16)
-    parser.add_argument("-s", "--start_offset", help="starting from i-th file in directory", type=int, default=0)
-    parser.add_argument("-e", "--end_offset", help="end processing at i-th file in directory", type=int, default=-1)
+    parser.add_argument("-s", "--start_offset", help="starting from i-th file in directory", type=int, default=45)
+    parser.add_argument("-e", "--end_offset", help="end processing at i-th file in directory", type=int, default=47)
     parser.add_argument("-d", "--raw_dir_path", help="directory where dataset stored", type=str, default='./dataset/raw_zinc_smiles')
     parser.add_argument("-o", "--output_dir_path", help="directory where processed data saved", type=str, default='./dataset/processed_zinc_smiles')
     parser.add_argument("-r", "--test_size", help="portion of validation_set", type=float, default=0.2)
