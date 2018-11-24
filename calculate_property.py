0440df973dc0c39069cefde739456082da3d6d21
Jaeyoung <whwodud9@gmail.com>
2018-11-25 03:20:51 +0900

    Add sampling feature.

--- calculate_property.py
+++ calculate_property.py
@@ -3,6 +3,7 @@ import os
 from os.path import isfile, join
 import time
 import argparse
+from random import sample
 from tqdm import tqdm
 from rdkit import Chem, DataStructs
 from rdkit.Chem import AllChem
@@ -10,7 +11,25 @@ from sklearn.model_selection import train_test_split
 from rdkit.Chem.Crippen import MolLogP, MolMR
 from rdkit.Chem.rdMolDescriptors import CalcTPSA, CalcPBF
 import pandas as df
+import re
 
+def extract_num(list_file):
+    list_num = list()
+    if len(list_file) == 0:
+        return 0
+    for file in list_file:
+        list_num.append(int([float(s) for s in re.findall(r'-?\d+\.?\d*', file)][0]))
+    return max(list_num)+1
+
+
+def get_last_num(output_dir_path='./dataset/processed_zinc_smiles'):
+    train_ouput_dir_path = join(output_dir_path, 'train')
+    val_output_dir_path = join(output_dir_path, 'val')
+    train_list_file = [f for f in os.listdir(train_ouput_dir_path) if isfile(join(train_ouput_dir_path, f))]
+    val_list_file = [f for f in os.listdir(val_output_dir_path) if isfile(join(val_output_dir_path, f))]
+    last_train_num = extract_num(train_list_file)
+    last_val_num = extract_num(val_list_file)
+    return last_train_num, last_val_num
 
 def process_smile(row):
     try:
@@ -20,25 +39,27 @@ def process_smile(row):
     except:
         return (None, None, None, None, None)
 
-def process_dataset(chunk_size=10000, #500000,
-                    num_worker=15,
-                    flag_continue=False,
-                    raw_dir_path='./dataset/raw_zinc_smiles',
-                    output_dir_path='./dataset/processed_zinc_smiles',
-                    start_offset=0, end_offset=-1, test_size=0.2):
+def process_dataset(chunk_size,
+                    num_worker,
+                    flag_continue,
+                    sampling_rate,
+                    test_size,
+                    raw_dir_path,
+                    output_dir_path,
+                    start_offset,
+                    end_offset):
     ts = time.time()
 
-    # Start Downloading Process
     list_file = [f for f in os.listdir(raw_dir_path) if isfile(join(raw_dir_path, f))]
-
     cnt_train_mol = 0
-    cnt_train_chunk = 0
     cnt_val_mol = 0
-    cnt_val_chunk = 0
+    if flag_continue:
+        cnt_train_chunk, cnt_val_chunk = get_last_num(output_dir_path)
+    else:
+        cnt_train_chunk, cnt_val_chunk = 0, 0
     train_row_buffer = list()
     val_row_buffer = list()
     label_columns = ('smile', 'logP', 'mr', 'tpsa', 'length')
-
     target_list_file = list_file[start_offset:end_offset]
 
 
@@ -46,16 +67,14 @@ def process_dataset(chunk_size=10000, #500000,
         m_ts = time.time()
         with open(join(raw_dir_path, filename)) as file:
             list_row = file.readlines()[1:]
+            sampled_list_row = sample(list_row, int(len(list_row)*sampling_rate))
             with mp.Pool(processes=num_worker) as pool:
-                data = pool.map(process_smile, list_row)
+                data = pool.map(process_smile, sampled_list_row)
         train_data, val_data = train_test_split(data,  test_size=test_size, random_state=111)
         train_row_buffer += train_data
         val_row_buffer += val_data
         cnt_train_mol += len(train_data)
         cnt_val_mol += len(val_data)
-        m_te = time.time()
-        print("Processed {:4}/{:4} files of {:8} mols took {:5.1f} sec. {:7.1f} mol/sec. Train: {:4} Val: {:4} Elap: {:6.1f} sec".format(
-            idx+1, len(target_list_file), len(data), m_te-m_ts, len(data)/(m_te-m_ts), cnt_train_chunk, cnt_val_chunk, time.time()-ts))
 
         while len(train_row_buffer) > chunk_size:
             if len(train_row_buffer) >= chunk_size:
@@ -74,7 +93,10 @@ def process_dataset(chunk_size=10000, #500000,
                 val_row_buffer = val_row_buffer[chunk_size:]
                 cnt_val_chunk += 1
 
-
+        m_te = time.time()
+        print("Processed {:4}/{:4} files of {:8} mols took {:5.1f} sec. {:7.1f} mol/sec. Train: {:4} Val: {:4} Elap: {:6.1f} sec".format(
+                idx + 1, len(target_list_file), len(data), m_te - m_ts, len(data) / (m_te - m_ts), cnt_train_chunk+1,
+                cnt_val_chunk+1, time.time() - ts))
 
     df_train = df.DataFrame.from_records(train_row_buffer, columns=label_columns)
     df_train = df_train.dropna()
@@ -98,20 +120,25 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Add logP, TPSA, MR, PBF value on .smi files')
     parser.add_argument("-c", "--chunk_size", help="number of rows in one chunk ", type=int, default=25000000)
     parser.add_argument("-n", "--num_worker", help="number of co-working process", type=int, default=16)
-    parser.add_argument("-s", "--start_offset", help="starting from i-th file in directory", type=int, default=45)
-    parser.add_argument("-e", "--end_offset", help="end processing at i-th file in directory", type=int, default=47)
+    parser.add_argument("-q", "--sampling_rate", help="number of co-working process", type=float, default=0.001)
+    parser.add_argument("-s", "--start_offset", help="starting from i-th file in directory", type=int, default=0)
+    parser.add_argument("-e", "--end_offset", help="end processing at i-th file in directory", type=int, default=-1)
     parser.add_argument("-d", "--raw_dir_path", help="directory where dataset stored", type=str, default='./dataset/raw_zinc_smiles')
-    parser.add_argument("-o", "--output_dir_path", help="directory where processed data saved", type=str, default='./dataset/processed_zinc_smiles')
+    parser.add_argument("-o", "--output_dir_path", help="directory where processed data saved", type=str, default='./dataset/processed_zinc_smiles/data_xs')
     parser.add_argument("-r", "--test_size", help="portion of validation_set", type=float, default=0.2)
     parser.add_argument("-t", "--flag_continue", help="whether continue writing file", type=bool, default=False)
 
     args = parser.parse_args()
 
+
     process_dataset(chunk_size=args.chunk_size,
                     num_worker=args.num_worker,
+                    sampling_rate=args.sampling_rate,
                     flag_continue=args.flag_continue,
                     raw_dir_path=args.raw_dir_path,
                     output_dir_path=args.output_dir_path,
                     start_offset=args.start_offset,
                     end_offset=args.end_offset,
                     test_size=args.test_size)
+
+
--- test.py
+++ test.py
@@ -1,15 +1,10 @@
 import pandas as pd
 from tqdm import tqdm
 import random
+import re
 
-temp_list = list()
-for i in tqdm(range(10000000)):
-    if random.random() > 0.1:
-        temp_list.append(("asdfasdfasdfasdfasdf", 0.234235, 234.23423, 54.234, 5))
-    else:
-        temp_list.append((None, 0.234235, 234.23423, 54.234, 5))
+def extract_num(st):
+    result = [int(s) for s in re.findall(r'-?\d+\.?\d*', st)][0]
+    return result
 
-
-b = pd.DataFrame.from_records(temp_list)
-b = b.dropna()
-print(b)
+print(extract_num('train 00001 .csv'))
