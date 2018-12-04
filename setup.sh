f69cc6fe3e8052924c7cb0f8aff2f03675f4af88
Jaeyoung <whwodud9@gmail.com>
2018-12-05 00:09:18 +0900

    Update install tensorboard script

--- setup.sh
+++ setup.sh
@@ -22,8 +22,9 @@ tar -zxvf data_m.tar.gz
 echo Extracting Complete!
 
 # Install dependency Package
-cd ../
+cd ../../
 pip install --upgrade pip
 pip install -r requirements.txt
 conda install pytorch-nightly -c pytorch
 pip install tensorflow
+git clone https://github.com/lanpa/tensorboardX && cd tensorboardX && python setup.py install
