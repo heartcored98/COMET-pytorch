4540c34751e6a5e1897fb4f1f72fbb60b5a849d2
Jaeyoung <whwodud9@gmail.com>
2018-12-06 02:42:58 +0900

    refactored code

--- dataloader.py
+++ dataloader.py
@@ -168,35 +168,7 @@ class BatchSampler(Sampler):
         else:
             return (len(self.sampler) + self.batch_size - 1) // self.batch_size
 
-"""
-class zincDataset(Dataset):
-    def __init__(self, data_path, skip_header=True):
-        self.data = pd.read_csv(data_path)
-        self.data = self.data.sort_values(by=['length'])
-        self.data = self.data.reset_index()
-        self.data['mr'] = np.log10(self.data['mr'] + 1)
-        self.data['tpsa'] = np.log10(self.data['tpsa'] + 1)
 
-        self.data = self.data.to_dict('index')
-
-    def __len__(self):
-        return len(self.data)
-    
-    def __getitem__(self, index):
-        row = self.data.loc[index, :]
-        smile = row.smile
-
-        mol = Chem.MolFromSmiles(smile)
-        adj = Chem.rdmolops.GetAdjacencyMatrix(mol)
-        list_feature = list()
-        for atom in mol.GetAtoms():
-            list_feature.append(atom_feature(atom))
-
-        return row.length, np.array(list_feature), adj, row.logP, row.mr, row.tpsa
-        
-    def get_sizes(self):
-        return self.data['length']
-"""
 class zincDataset(Dataset):
     def __init__(self, data_path, skip_header=True):
         self.data = pd.read_csv(data_path)
